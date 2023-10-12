4fcaa52d2a44a247333537d1305ef5b16793b3d9
preetamburla <145477066+preetamburla@users.noreply.github.com>
2023-10-12 23:11:24 +0530

    Update TechEvents.jsx

--- src/Pages/TechEvents/TechEvents.jsx
+++ src/Pages/TechEvents/TechEvents.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./TechEvents.css"
+import RendeerObjects from './data.jsx'
+import Contact from '../../Components/Contact/Contact';
+
 function TechEvents() {
   return (
-    <div>TechEvents</div>
-  )
+    <div>
+           <h1>Tech Events</h1> 
+           <RendeerObjects />
+           <Contact/>
+    </div>
+  );
 }
 
 export default TechEvents
